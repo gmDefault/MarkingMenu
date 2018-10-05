8dd695a2ea5cd9ce0252851af73758da013566fd
gmDefault <cedric.lafrasse@gmail.com>
2018-10-05 11:44:53 +0200

    Version with Ellipse and colors (handle multiples colors)

--- src/Paint.java
+++ src/Paint.java
@@ -8,9 +8,15 @@
 
 import static java.lang.Math.*;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.Vector;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
@@ -18,7 +24,7 @@ import java.awt.Point;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.RenderingHints;
-
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Rectangle2D;
 
@@ -28,6 +34,7 @@ import javax.swing.event.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 
@@ -35,8 +42,8 @@ import javax.swing.SwingUtilities;
 /* paint *******************************************************************/
 
 class Paint extends JFrame {
-	Vector<Shape> shapes = new Vector<Shape>();
-
+	Map<Shape, Color> shapes = new HashMap();
+	static Color color = new Color(0);
 	class Tool extends AbstractAction
 	           implements MouseInputListener {
 	   Point o;
@@ -62,11 +69,11 @@ class Paint extends JFrame {
 	Tool tools[] = {
 		new Tool("pen") {
 			public void mouseDragged(MouseEvent e) {
-				Path2D.Double path = (Path2D.Double)shape;
+				Path2D.Double path = (Path2D.Double) shape;
 				if(path == null) {
 					path = new Path2D.Double();
 					path.moveTo(o.getX(), o.getY());
-					shapes.add(shape = path);
+					shapes.put(shape = path, color);
 				}
 				path.lineTo(e.getX(), e.getY());
 				panel.repaint();
@@ -77,17 +84,34 @@ class Paint extends JFrame {
 				Rectangle2D.Double rect = (Rectangle2D.Double)shape;
 				if(rect == null) {
 					rect = new Rectangle2D.Double(o.getX(), o.getY(), 0, 0);
-					shapes.add(shape = rect);
+					shapes.put(shape = rect, color);
 				}
 				rect.setRect(min(e.getX(), o.getX()), min(e.getY(), o.getY()),
 				             abs(e.getX()- o.getX()), abs(e.getY()- o.getY()));
 				panel.repaint();
 			}
-		}
+		},
+		new Tool("ellipse") {
+			public void mouseDragged(MouseEvent e) {
+				Ellipse2D.Double ellipse = (Ellipse2D.Double)shape;  
+				if(ellipse == null) {
+					ellipse = new Ellipse2D.Double(o.getX(), o.getY(), 0, 0);
+					shapes.put(shape = ellipse, color);
+				}
+				ellipse.setFrame(min(e.getX(), o.getX()), min(e.getY(), o.getY()),
+				             abs(e.getX()- o.getX()), abs(e.getY()- o.getY()));
+				panel.repaint();
+			}
+		},
 	};
+		
 	Tool tool;
 
 	JPanel panel;
+	
+
+	
+
 	
 	public Paint(String title) {
 		super(title);
@@ -97,20 +121,50 @@ class Paint extends JFrame {
 			for(AbstractAction tool: tools) {
 				add(tool);
 			}
+			JButton blue = new JButton("blue");
+			blue.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					color = Color.BLUE;
+				} 
+			});
+			add(blue);
+			JButton red = new JButton("red");
+			red.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					color = Color.RED;
+				} 
+			});
+			add(red);
+			JButton green = new JButton("green");
+			green.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					color = Color.GREEN;
+				} 
+			});
+			add(green);
+
 		}}, BorderLayout.NORTH);
+
+		
 		add(panel = new JPanel() {	
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);	
 				Graphics2D g2 = (Graphics2D)g;
+
 				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
 				                    RenderingHints.VALUE_ANTIALIAS_ON);
 		
 				g2.setColor(Color.WHITE);
 				g2.fillRect(0, 0, getWidth(), getHeight());
 				
-				g2.setColor(Color.BLACK);
-				for(Shape shape: shapes) {
-					g2.draw(shape);
+				g2.setColor(color);
+				
+				Set<Entry<Shape, Color>> setHm = shapes.entrySet();
+				Iterator<Entry<Shape, Color>> it = setHm.iterator();
+				while(it.hasNext()) {
+					Entry<Shape, Color> e = it.next();
+					g2.setColor(e.getValue());
+					g2.draw(e.getKey());
 				}
 			}
 		});
